af5cb07793666850dd142e670aa6a2ff3f9263ff
katsuya53 <redstar53hk@gmail.com>
2024-03-05 22:21:31 +0900

    コントローラー作成

--- db/migrate/20240305131720_create_blogs.rb
+++ db/migrate/20240305131720_create_blogs.rb
@@ -1,12 +1,10 @@
-class CreateBlogs < ActiveRecord::Migration[5.2]
+class CreateBlogs < ActiveRecord::Migration[7.0]
   def change
     create_table :blogs do |t|
-#ここから
       t.string :title
       t.text :content
       t.datetime :start_time
-#ここまでを追加
-      
       t.timestamps
     end
   end
+end
